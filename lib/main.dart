82794655366c89b5a660b7708f7e3eda13cf6025
Yuto <63772196+seekrett@users.noreply.github.com>
2026-03-18 17:17:48 -0400

    fixed merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< steps-counter-widget
 import 'steps.dart';                      // step counter stuff
-=======
 import 'package:walkyourcat/navbar.dart';
 
 enum SampleItem { itemOne, itemTwo, itemThree }
->>>>>>> main
 
 void main() {
   runApp(const MyApp());
@@ -97,26 +94,10 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-<<<<<<< steps-counter-widget
       body: Stack(
         children: [
           Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
             child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
-              //
-              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-              // action in the IDE, or press "p" in the console), to see the
-              // wireframe for each widget.
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const Text(
@@ -138,23 +119,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),  
           
         ],
-=======
-
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 16),
-            Text(_selectedMenuLabel),
-          ],
-        ),
->>>>>>> main
       ),
 
 
