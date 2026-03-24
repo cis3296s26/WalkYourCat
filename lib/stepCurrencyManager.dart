41b39cbad6e0bf576bcf222a5ba06a3c94bad8af
Yuto <63772196+seekrett@users.noreply.github.com>
2026-03-24 13:25:58 -0400

    added shared preferences dependencies

---
+++ lib/stepCurrencyManager.dart
@@ -0,0 +1,11 @@
+// IMPORT STATEMENTS
+import 'package:shared_preferences/shared_preferences.dart';    // for storing coin balance persistently
+
+class StepCurrencyManager {
+  // CODE HERE
+
+  // functions to implement
+  // - loadCoins()
+  // - saveCoins()
+  // - processNewSteps(int steps)
+}
