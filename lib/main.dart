52e67bcb6aed2ce1c552c94d922474fde2f8051d
tut08455 <228950673+tut08455@users.noreply.github.com>
2026-04-15 21:44:05 -0400

    removed stale dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'dart:async';
 import 'package:audioplayers/audioplayers.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:walkyourcat/features/achievements/achievements_modal.dart';
