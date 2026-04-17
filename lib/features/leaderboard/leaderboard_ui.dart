47dde56458a4b4d4c6ffe72d2b2b6ac314e9eb7c
adi <tut25650@temple.edu>
2026-04-17 08:56:08 -0400

    added leaderboard ui with user functionality

--- lib/features/leaderboard/leaderboard_ui.dart
+++ lib/features/leaderboard/leaderboard_ui.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:walkyourcat/stepcurrency_manager.dart';
 
 
-class LeaderboardDialog extends StatelessWidget {
+class LeaderboardDialog extends StatefulWidget {
   const LeaderboardDialog({super.key});
+
+  @override
+  State<LeaderboardDialog> createState() => _LeaderboardDialogState();
+}
+
+class _LeaderboardDialogState extends State<LeaderboardDialog> {
+  late Future<int> _stepsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _stepsFuture = _loadSteps();
+  }
+
+  Future<int> _loadSteps() async {
+    final manager = StepCurrencyManager();
+    await manager.loadState();
+    return manager.unprocessedSteps;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,21 +37,152 @@ class LeaderboardDialog extends StatelessWidget {
       child: SizedBox(
         width: dialogWidth,
         height: dialogHeight,
-        // child: FutureBuilder<List<Leaderboard>>(
-        //   future: LeaderboardsService.instance.getAllLeaderboards(),
-        //   builder: (context, snapshot) {
-        //     if (snapshot.connectionState != ConnectionState.done) {
-        //       return const _LeaderboardLoadingCard();
-        //     }
+        child: FutureBuilder<int>(
+          future: _stepsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return _LeaderboardCard(steps: 0);
+            }
+            return _LeaderboardCard(steps: snapshot.data ?? 0);
+          },
+        ),
+      ),
+    );
+  }
+}
 
-        //     final Leaderboards = snapshot.data ?? <Leaderboard>[];
-        //     if (Leaderboards.isEmpty) {
-        //       return const _LeaderboardEmptyCard();
-        //     }
+class _LeaderboardCard extends StatelessWidget {
+  final int steps;
 
-        //     return _LeaderboardModalCard(Leaderboard: Leaderboard);
-        //   },
-        // ),
+  const _LeaderboardCard({required this.steps});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.leaderboard,
+                      size: 28,
+                      color: Colors.blue.shade700,
+                    ),
+                    const SizedBox(width: 8),
+                    const Text(
+                      'Leaderboard',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView(
+                children: [
+                  _LeaderboardEntryTile(
+                    rank: 1,
+                    name: 'You',
+                    points: steps,
+                    isCurrentUser: true,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _LeaderboardEntryTile extends StatelessWidget {
+  final int rank;
+  final String name;
+  final int points;
+  final bool isCurrentUser;
+
+  const _LeaderboardEntryTile({
+    required this.rank,
+    required this.name,
+    required this.points,
+    this.isCurrentUser = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: isCurrentUser ? Colors.blue.shade50 : Colors.grey.shade50,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(
+          color: isCurrentUser ? Colors.blue.shade300 : Colors.grey.shade300,
+          width: isCurrentUser ? 2 : 1,
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 32,
+                height: 32,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade700,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Center(
+                  child: Text(
+                    '#$rank',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 12),
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: isCurrentUser ? FontWeight.bold : FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            '$points steps',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue.shade700,
+            ),
+          ),
+        ],
       ),
     );
   }
