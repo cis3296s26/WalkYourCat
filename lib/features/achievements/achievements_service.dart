2f3b8b78397d17159b8d7d6a45c43da2f13e420d
tut08455 <228950673+tut08455@users.noreply.github.com>
2026-04-18 18:19:57 -0400

    consolidating achievements service logic

--- lib/features/achievements/achievements_service.dart
+++ lib/features/achievements/achievements_service.dart
@@ -1,46 +1,14 @@
-import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
+import 'package:walkyourcat/core/services/database_service.dart';
 import 'package:walkyourcat/features/challenges/challenge_item.dart';
 import 'achievement_item.dart';
 
 class AchievementsService {
   static final AchievementsService instance = AchievementsService._init();
-  static Database? _database;
 
   AchievementsService._init();
 
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDB('achievements_history.db');
-    return _database!;
-  }
-
-  Future<Database> _initDB(String filePath) async {
-    String path;
-    if (kIsWeb) {
-      path = filePath;
-    } else {
-      final dbPath = await getDatabasesPath();
-      path = join(dbPath, filePath);
-    }
-    return await openDatabase(
-      path,
-      version: 1,
-      onCreate: _createDB,
-    );
-  }
-
-  Future _createDB(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE achievements (
-        id TEXT PRIMARY KEY,
-        title TEXT NOT NULL,
-        type TEXT NOT NULL,
-        completionCount INTEGER NOT NULL DEFAULT 0
-      )
-    ''');
-  }
+  Future<Database> get database async => await DatabaseService.instance.database;
 
   Future<void> recordChallengeCompletion(Challenge challenge) async {
     final db = await instance.database;
