d50b917186cd7c5d3e3f4f1bbf3c7c571c41a2ec
adi <tut25650@temple.edu>
2026-04-24 10:47:26 -0400

    modified leaderboard class to enable leaderboard functionality (for bots)

--- lib/features/leaderboard/leaderboard_item.dart
+++ lib/features/leaderboard/leaderboard_item.dart
@@ -9,5 +9,19 @@ class Leaderboard {
     required this.stepsCount,
   });
 
+  factory Leaderboard.fromMap(Map<String, dynamic> map) {
+    return Leaderboard(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      stepsCount: map['stepsCount'] as int,
+    );
+  }
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'stepsCount': stepsCount,
+    };
+  }
 }
